203247345cf15fcee101bcda29c361a69e1dd545
gyuri <rbfl7645@naver.com>
2024-05-15 00:43:56 +0900

    first django project

---
+++ webApp/users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def login(request):
+    return render(request, "login.html", {})
+def signup(request):
+    return render(request, "signup.html", {})
+
+# Create your views here.
---
+++ webApp/webApp/localsettings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME' : 'django',
+        'USER' : 'gyuri',
+        'PASSWORD' : 'gyuri',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-s5u97-@=mc0%bm0siri63le1^1viy3b9q#$2+86bl+@cxwqhqp'
